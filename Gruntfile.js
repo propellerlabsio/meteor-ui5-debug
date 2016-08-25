a9ebb3f390b233b1dae297d2000debffbcf863e7
Peter Roehlen <peter.roehlen@gmail.com>
2016-08-25 12:24:07 +0700

    Split out meteor-ui5-debug from old meteor-ui5 v1 branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,50 +16,21 @@ module.exports = function(grunt) {
           configure: "jsdoc.conf.json"
         }
       }
-    },
-    babel: {
-      options: {
-        sourceMap: true,
-        presets: ['es2015']
-      },
-      babel_out: {
-        files: [{
-          "expand": true,
-          "cwd": "src/ui5",
-          "src": ["**/*.js"],
-          "dest": "temp/babel_out",
-          "ext": ".js"
-        }]
-      }
-    },
-    uglify: {
-      dist: {
-        files: [{
-          "expand": true,
-          "cwd": "temp/babel_out",
-          "src": ["**/*.js"],
-          "dest": "dist",
-        }]
-      }
     }
   });
 
   // Load grunt plugin tasks from pre-installed npm packages
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-babel');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
-  // Local task: Create meteor package file
+  // Local task: Copy README
   grunt.registerTask(
-    'create_package_js',
-    'Create meteor package file',
+    'create_readme',
+    'Copy README to package dist',
     function() {
-      // TODO rather than just copy package.js, build list of files for
-      // api.addFiles() method based on current UI5 models and controls.
-      grunt.file.copy('src/package.js', 'dist/package.js');
-      grunt.log.ok('Meteor package.js copied from src/.');
+      grunt.file.copy('README.md', 'dist/README.md');
+      grunt.log.ok('Copied README to package dist.');
     }
   );
 
@@ -70,69 +41,8 @@ module.exports = function(grunt) {
     'copy_meteor_sources_to_dist',
     'Copying meteor sources to dist.',
     function() {
-      grunt.file.copy('src/meteor', 'dist/meteor');
+      grunt.file.copy('src', 'dist');
       grunt.log.ok('Meteor sources copied unprocessed.');
-    }
-  );
-
-
-  // Local task: Create UI5 debug files
-  grunt.registerTask(
-    'create_ui5_debug_files',
-    'Create UI5 debug files ("-dbg")',
-    function() {
-      // TODO move this into outside function so the grunt file is easier to
-      // to follow. I tried but couldn't get a reference to the grunt object
-      var debugFilesCreated = 0;
-      var sourceMapsCopied = 0;
-
-      // Recurse babel output directory copying unminfied javascript to dist
-      // directory but with "-dbg" in the name before the first dot.
-      grunt.file.recurse(
-        'temp/babel_out',
-        function(abspath, root, subdir, filename) {
-          // Ignore root directory
-          if (subdir) {
-            // Only create -dbg versions of javascript files
-            if (filename.endsWith(".js")) {
-              // Source file is absolute path
-              var sourceFile = abspath;
-
-              // Destination file name has "-dbg" in the filename before first period
-              var firstPeriod = filename.indexOf('.');
-              var destFileName = [
-                filename.slice(0, firstPeriod),
-                '-dbg',
-                filename.slice(firstPeriod)
-              ].join('');
-
-              // Destination is in 'dist' directory
-              var destFile = 'dist/' + subdir + '/' + destFileName;
-
-              // Copy file
-              grunt.file.copy(sourceFile, destFile);
-              debugFilesCreated++;
-            } else if (filename.endsWith(".map")) {
-              // Just copy source maps as is.  Babel includes the original
-              // name at the bottom of the javascript file.
-              var sourceFile = abspath;
-
-              // Destination is in 'dist' directory
-              var destFile = 'dist/' + subdir + '/' + filename;
-
-              // Copy file
-              grunt.file.copy(sourceFile, destFile);
-              sourceMapsCopied++;
-            }
-          }
-        }
-      );
-
-      // Finished
-      grunt.log.ok(
-        debugFilesCreated + ' debug files created. ' +
-        sourceMapsCopied + ' source maps copied.'
-      );
     }
   );
 
@@ -141,11 +51,8 @@ module.exports = function(grunt) {
     'eslint',
     'clean',
     'jsdoc',
-    'babel',
-    'uglify',
     'copy_meteor_sources_to_dist',
-    'create_package_js',
-    'create_ui5_debug_files'
+    'create_readme'
   ]);
 
 };
--- src/package.js
+++ src/package.js
@@ -3,17 +3,17 @@
  * @copyright PropellerLabs.io 2016
  * @license Apache-2.0
  *
- * @namespace meteor-ui5
- * @description The Meteor UI5 package
+ * @namespace meteor-ui5-debug
+ * @description The Meteor UI5 Debug package
  */
 
 /* globals Package */
 
 Package.describe({
-  name: 'propellerlabsio:meteor-ui5',
+  name: 'propellerlabsio:meteor-ui5-debug',
   version: '0.1.0',
-  summary: 'UI5 with Meteor',
-  git: 'https://github.com/propellerlabsio/meteor-ui5',
+  summary: 'Debugging helper for building web apps using UI5.',
+  git: 'https://github.com/propellerlabsio/meteor-ui5-debug',
   documentation: 'README.md'
 });
 
@@ -21,38 +21,9 @@ Package.onUse((api) => {
   api.versionsFrom('1.4.0.1');
   api.use('ecmascript');
 
-  // Add package so we can reference Mongo collections by name, imply it
-  // so UI5 app builders can do the same.
-  api.use('dburles:mongo-collection-instances');
-  api.imply('dburles:mongo-collection-instances');
-
   // Add a convience web app connect handler to allow user to debug their
   // UI5 apps using the normal UI5 debug option as long as they build their apps
   // in the public/webapp folder.
   api.use('webapp', 'server');
-  api.addFiles('meteor/server/ui5DebugHandler.js', 'server');
-
-  // Add our model and control files. Note OpenUI5 requires these files to be
-  // served as is with none of meteor's mushing which is why we use the bare and
-  // isAsset options.
-  api.addFiles([
-    'model/mongo/ContextBinding.js',
-    'model/mongo/ContextBinding-dbg.js',
-    'model/mongo/ContextBinding.js.map',
-    'model/mongo/DocumentListBinding.js',
-    'model/mongo/DocumentListBinding-dbg.js',
-    'model/mongo/DocumentListBinding.js.map',
-    'model/mongo/Model.js',
-    'model/mongo/Model-dbg.js',
-    'model/mongo/Model.js.map',
-    'model/mongo/PropertyBinding.js',
-    'model/mongo/PropertyBinding-dbg.js',
-    'model/mongo/PropertyBinding.js.map',
-    'model/mongo/PropertyListBinding.js',
-    'model/mongo/PropertyListBinding-dbg.js',
-    'model/mongo/PropertyListBinding.js.map'
-  ], 'client', {
-    bare: true,
-    isAsset: true // Allows clients to reference model by <resourcepath>.model.mongo.Model
-  });
+  api.addFiles('server/ui5DebugHandler.js', 'server');
 });
